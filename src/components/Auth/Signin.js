330283078964d4920c306c996d5ea3666cb791a5
Lailz <lailaabdulraheem@gmail.com>
2022-04-20 01:56:37 +0300

    cheange email to username in signin form

--- src/components/Auth/Signin.js
+++ src/components/Auth/Signin.js
@@ -55,10 +55,10 @@ function Signin() {
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
               autoFocus
             />
             <TextField
