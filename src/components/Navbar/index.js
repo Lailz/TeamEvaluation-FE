708e1017a8b54e04b8a67c8dde288617a82c8942
Lailz <lailaabdulraheem@gmail.com>
2022-04-20 05:08:32 +0300

    remove profile section and add signout to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,33 +1,25 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+// Actions
+import { signout } from "../../store/slices/authSlice";
 
 // Material UI
-import AccountCircle from "@mui/icons-material/AccountCircle";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { useDispatch } from "react-redux";
-import { signout } from "../../store/slices/authSlice";
 
 function Navbar() {
-  const [anchorEl, setAnchorEl] = useState(null);
   const dispatch = useDispatch();
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.authReducer.user);
 
   const handleSignout = () => {
     dispatch(signout());
-    handleClose();
+    navigate("/signin");
   };
 
   return (
@@ -46,36 +38,11 @@ function Navbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Team Evaluation
           </Typography>
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleSignout}>Sign out</MenuItem>
-            </Menu>
-          </div>
+          {user && (
+            <div>
+              <Typography onClick={handleSignout}>Sign out</Typography>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
