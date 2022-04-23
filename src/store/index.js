36655c46ec653c5ce30df91b9bf9a3fdaad7f688
Lailz <lailaabdulraheem@gmail.com>
2022-04-23 21:09:51 +0300

    move projects to its own reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer, { checkForToken } from "./slices/authSlice";
 import criteriaReducer, { fetchCriteriaList } from "./slices/criteriaSlice";
+import projectReducer, { fetchProjectList } from "./slices/projectSlice";
 import semesterReducer from "./slices/semesterSlice";
 
 export const store = configureStore({
   reducer: {
     authReducer: authReducer,
     criteriaReducer: criteriaReducer,
+    projectReducer: projectReducer,
     semesterReducer: semesterReducer,
   },
 });
 
 store.dispatch(checkForToken());
 store.dispatch(fetchCriteriaList());
+store.dispatch(fetchProjectList());
