a30330948b5ce2d271c8117cf3ba6e6da0d5c389
Lailz <lailaabdulraheem@gmail.com>
2022-04-23 22:13:39 +0300

    create team reducer

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import authReducer, { checkForToken } from "./slices/authSlice";
 import criteriaReducer, { fetchCriteriaList } from "./slices/criteriaSlice";
 import projectReducer, { fetchProjectList } from "./slices/projectSlice";
 import semesterReducer from "./slices/semesterSlice";
+import teamReducer, { fetchTeamList } from "./slices/teamSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,9 +11,11 @@ export const store = configureStore({
     criteriaReducer: criteriaReducer,
     projectReducer: projectReducer,
     semesterReducer: semesterReducer,
+    teamReducer: teamReducer,
   },
 });
 
 store.dispatch(checkForToken());
 store.dispatch(fetchCriteriaList());
 store.dispatch(fetchProjectList());
+store.dispatch(fetchTeamList());
---
+++ src/store/slices/teamSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../api";
+
+const initialState = {
+  teams: [],
+  loading: true,
+};
+
+export const fetchTeamList = createAsyncThunk(
+  "team/list",
+  async (_, thunkAPI) => {
+    try {
+      const res = await api.get("/teams/");
+      return res.data;
+    } catch (error) {
+      thunkAPI.rejectWithValue("Oops! something went wrong.");
+    }
+  }
+);
+
+export const createTeam = createAsyncThunk(
+  "team/create",
+  async ({ project, team, handleClose }, thunkAPI) => {
+    try {
+      const res = await api.post(`/projects/${project.id}/teams/`, team);
+      handleClose();
+      return res.data;
+    } catch (error) {
+      thunkAPI.rejectWithValue("Oops! something went wrong.");
+    }
+  }
+);
+
+export const teamSlice = createSlice({
+  name: "team",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchTeamList.fulfilled, (state, action) => {
+      state.teams = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(createTeam.fulfilled, (state, action) => {
+      state.teams.push(action.payload);
+    });
+  },
+});
+
+// export const {  } = semesterSlice.actions;
+
+export default teamSlice.reducer;
