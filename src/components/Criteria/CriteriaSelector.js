b1b28f063670242c09a270b3c0a1636317e0c1a9
Lailz <lailaabdulraheem@gmail.com>
2022-04-24 03:06:38 +0300

    change criteria select from mui to react-select

--- src/components/Criteria/CriteriaSelector.js
+++ src/components/Criteria/CriteriaSelector.js
@@ -1,66 +1,35 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import Select from "react-select";
 
 // Components
 import SelectedCriteria from "./SelectedCriteria";
 
 // MUI
-import { useTheme } from "@mui/material/styles";
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  OutlinedInput,
-  Select,
-} from "@mui/material";
+import { FormControl, InputLabel } from "@mui/material";
 
 // Styles
 import { getStyles, MenuProps } from "./styles";
 
 function CriteriaSelector() {
-  const theme = useTheme();
-  const [personName, setPersonName] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-  const criteriaList = useSelector((state) =>
-    state.criteriaReducer.criterias.map(
-      (criteria) => `${criteria.name} - ${criteria.weight}`
-    )
+  const criteriaOptions = useSelector((state) =>
+    state.criteriaReducer.criterias.map((criteria) => ({
+      value: criteria.id,
+      label: `${criteria.name} - ${criteria.weight}`,
+    }))
   );
 
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPersonName(typeof value === "string" ? value.split(",") : value);
-  };
-  console.log(
-    "🚀 ~ file: CriteriaSelector.js ~ line 56 ~ CriteriaSelector ~ MenuProps",
-    MenuProps
-  );
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-chip-label">Select Criteria</InputLabel>
         <Select
-          labelId="demo-multiple-chip-label"
-          id="demo-multiple-chip"
-          multiple
-          value={personName}
-          onChange={handleChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => <SelectedCriteria selected={selected} />}
-          MenuProps={MenuProps}
-        >
-          {criteriaList.map((criteria) => (
-            <MenuItem
-              key={criteria}
-              value={criteria}
-              style={getStyles(criteria, personName, theme)}
-            >
-              {criteria}
-            </MenuItem>
-          ))}
-        </Select>
+          isMulti
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={criteriaOptions}
+        />
       </FormControl>
     </div>
   );
