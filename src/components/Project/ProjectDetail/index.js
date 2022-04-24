42bde75635f4ec9851f895e524c0e5e1dad4a82a
Lailz <lailaabdulraheem@gmail.com>
2022-04-24 05:39:19 +0300

    add table header

--- src/components/Project/ProjectDetail/index.js
+++ src/components/Project/ProjectDetail/index.js
@@ -2,7 +2,14 @@ import { useParams } from "react-router";
 import { useSelector } from "react-redux";
 
 // MUI
-import { Container } from "@mui/material";
+import {
+  Container,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import TeamFilter from "./TeamFilter";
 import Loading from "../../Loading";
 
@@ -10,13 +17,31 @@ const ProjectDetail = () => {
   const { projectSlug, teamSlug } = useParams();
   const projects = useSelector((state) => state.projectReducer.projects);
   const loading = useSelector((state) => state.projectReducer.loading);
+  const teams = useSelector((state) => state.teamReducer.teams);
+  let foundTeam = null;
   if (loading) return <Loading />;
   const project = projects.find((_project) => _project.slug === projectSlug);
+
+  if (teamSlug) {
+    foundTeam = teams.find((team) => team.slug === teamSlug);
+  }
   return (
     <Container>
       <h2>{project.name}</h2>
       <h4>{project.semester}</h4>
       <TeamFilter project={project} />
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Criteria</TableCell>
+              <TableCell>Weight</TableCell>
+              <TableCell>Avg. Score</TableCell>
+              <TableCell>Weighted Avg.</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
